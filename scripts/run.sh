a12b81c0682ed8d84fa7d3a76e13521889172274
Eric Rosas <ericdrosas@gmail.com>
2021-06-10 10:31:59 -0700

    Adding missing dependencies installation step

--- scripts/run.sh
+++ scripts/run.sh
@@ -29,5 +29,31 @@ if [[ "$sql_file" ]]; then
 fi
 echo "Done importing DB!"
 
+cd /var/www/html/craftcms
+
+echo 'Installing composer dependencies.'
+composer install
+
+dependencies=$(cat composer.json |
+    jq '.require' |
+    jq --compact-output 'keys' |
+    tr -d '[]"' | tr ',' '\n')
+
+for package in ${dependencies}; do
+
+    echo $package
+
+    vendor=$(awk -F '[\/:]+' '{print $1}' <<<$package)
+    packageName=$(awk -F '[\/:]+' '{print $2}' <<<$package)
+    isCraftPlugin=$(cat vendor/$vendor/$packageName/composer.json | jq '.type == "craft-plugin"')
+
+    if [ "$isCraftPlugin" = true ]; then
+        handle=$(cat vendor/$vendor/$packageName/composer.json | jq -r '.extra.handle')
+        ./craft install/plugin $handle
+    fi
+done
+
+echo '✅  All dependencies successfully installed.'
+
 # Start php-fpm
 exec "$@"
