072820e8ef1445e911127562adc17c717920f391
Blake Mason <bmason@lsst.org>
2021-06-10 17:29:53 -0700

    [C] Basic setup
     - Multi-site: english & spanish
     - GraphQL endpoint
     - configs

--- craftcms/config/general.php
+++ craftcms/config/general.php
@@ -14,24 +14,45 @@ $isDev = App::env('ENVIRONMENT') === 'dev';
 $isProd = App::env('ENVIRONMENT') === 'production';
 
 return [
-    // Default Week Start Day (0 = Sunday, 1 = Monday...)
-    'defaultWeekStartDay' => 1,
+    '*' => [
+        // Default Week Start Day (0 = Sunday, 1 = Monday...)
+        'defaultWeekStartDay' => 1,
 
-    // Whether generated URLs should omit "index.php"
-    'omitScriptNameInUrls' => true,
+        // Whether generated URLs should omit "index.php"
+        'omitScriptNameInUrls' => true,
 
-    // The URI segment that tells Craft to load the control panel
-    'cpTrigger' => App::env('CP_TRIGGER') ?: 'admin',
+        // Control Panel trigger word
+        'cpTrigger' => 'admin',
 
-    // The secure key Craft will use for hashing and encrypting data
-    'securityKey' => App::env('SECURITY_KEY'),
+        // The secure key Craft will use for hashing and encrypting data
+        'securityKey' => App::env('SECURITY_KEY'),
 
-    // Whether Dev Mode should be enabled (see https://craftcms.com/guides/what-dev-mode-does)
-    'devMode' => $isDev,
+        // Whether to save the project config out to config/project.yaml
+        // (see https://docs.craftcms.com/v3/project-config.html)
+        'useProjectConfigFile' => true,
 
-    // Whether administrative changes should be allowed
-    'allowAdminChanges' => $isDev,
+        'headlessMode' => true,
 
-    // Whether crawlers should be allowed to index pages and following links
-    'disallowRobots' => !$isProd,
+        'aliases' => [
+            '@webBaseUrl' => App::env('WEB_BASE_URL')
+        ],
+    ],
+
+    // Dev environment settings
+    'dev' => [
+        // Dev Mode (see https://craftcms.com/guides/what-dev-mode-does)
+        'devMode' => App::env('ENVIRONMENT') === 'dev',
+    ],
+
+    // Staging environment settings
+    'staging' => [
+        // Set this to `false` to prevent administrative changes from being made on staging
+        'allowAdminChanges' => false,
+    ],
+
+    // Production environment settings
+    'production' => [
+        // Set this to `false` to prevent administrative changes from being made on production
+        'allowAdminChanges' => false,
+    ],
 ];
