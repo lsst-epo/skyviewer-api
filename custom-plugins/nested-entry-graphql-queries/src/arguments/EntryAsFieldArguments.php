1340cba6a7bbdba99a179303eb1c3a79cd322c54
Eric Rosas <ericdrosas@gmail.com>
2021-08-30 11:28:49 -0700

    Added collection handler function to arg handler EPO-5172

--- custom-plugins/nested-entry-graphql-queries/src/arguments/EntryAsFieldArguments.php
+++ custom-plugins/nested-entry-graphql-queries/src/arguments/EntryAsFieldArguments.php
@@ -10,11 +10,9 @@ class EntryAsFieldArguments extends \craft\gql\base\ElementArguments
 {
     public static function getArguments(): array
     {
-        // append our argument to common element arguments and any from custom fields
         return array_merge(parent::getArguments(), self::getContentArguments(), [
             'searchRelated' => [
-                'name' => 'searchRelated', // this name
-                //'type' => Type::listOf(Type::string()),  Type::listOf(QueryArgument::getType()),
+                'name' => 'searchRelated',
                 'type' => Type::listOf(Entry::getType()),
                 'description' => 'Matches search on entries-as-fields and returns the whole entry.'
             ],
