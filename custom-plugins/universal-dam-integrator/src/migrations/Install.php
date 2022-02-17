0aa1912286c361339c00a78269d08e3b1252886f
Eric Rosas <ericdrosas@gmail.com>
2022-02-17 16:06:08 -0700

    Adding DAM integration plugin logic EPO-5780

---
+++ custom-plugins/universal-dam-integrator/src/migrations/Install.php
@@ -0,0 +1,187 @@
+<?php
+/**
+ * Universal DAM Integrator plugin for Craft CMS 3.x
+ *
+ * Some description
+ *
+ * @link      www.lsst.org
+ * @copyright Copyright (c) 2022 Eric Rosas
+ */
+
+namespace rosas\dam\migrations;
+
+// use \universaldamintegrator\UniversalDamIntegrator;
+
+use Craft;
+use craft\config\DbConfig;
+use craft\db\Migration;
+
+/**
+ * Universal DAM Integrator Install Migration
+ *
+ * If your plugin needs to create any custom database tables when it gets installed,
+ * create a migrations/ folder within your plugin folder, and save an Install.php file
+ * within it using the following template:
+ *
+ * If you need to perform any additional actions on install/uninstall, override the
+ * safeUp() and safeDown() methods.
+ *
+ * @author    Eric Rosas
+ * @package   UniversalDamIntegrator
+ * @since     1.0.0
+ */
+class Install extends Migration
+{
+    // Public Properties
+    // =========================================================================
+
+    /**
+     * @var string The database driver to use
+     */
+    public $driver;
+
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * This method contains the logic to be executed when applying this migration.
+     * This method differs from [[up()]] in that the DB logic implemented here will
+     * be enclosed within a DB transaction.
+     * Child classes may implement this method instead of [[up()]] if the DB logic
+     * needs to be within a transaction.
+     *
+     * @return boolean return a false value to indicate the migration fails
+     * and should not proceed further. All other return values mean the migration succeeds.
+     */
+    public function safeUp()
+    {
+        $this->driver = Craft::$app->getConfig()->getDb()->driver;
+        if ($this->createTables()) {
+            $this->createIndexes();
+            $this->addForeignKeys();
+            // Refresh the db schema caches
+            Craft::$app->db->schema->refresh();
+            $this->insertDefaultData();
+        }
+
+        return true;
+    }
+
+    /**
+     * This method contains the logic to be executed when removing this migration.
+     * This method differs from [[down()]] in that the DB logic implemented here will
+     * be enclosed within a DB transaction.
+     * Child classes may implement this method instead of [[down()]] if the DB logic
+     * needs to be within a transaction.
+     *
+     * @return boolean return a false value to indicate the migration fails
+     * and should not proceed further. All other return values mean the migration succeeds.
+     */
+    public function safeDown()
+    {
+        $this->driver = Craft::$app->getConfig()->getDb()->driver;
+        $this->removeTables();
+
+        return true;
+    }
+
+    // Protected Methods
+    // =========================================================================
+
+    /**
+     * Creates the tables needed for the Records used by the plugin
+     *
+     * @return bool
+     */
+    protected function createTables()
+    {
+        $tablesCreated = false;
+
+    // universaldamintegrator_universaldamintegrator table
+        $tableSchema = Craft::$app->db->schema->getTableSchema('{{%universaldamintegrator_asset_metadata}}');
+        if ($tableSchema === null) {
+            $tablesCreated = true;
+            $this->createTable(
+                '{{%universaldamintegrator_asset_metadata}}',
+                [
+                    'id' => $this->primaryKey(),
+                    'dateCreated' => $this->dateTime()->notNull(),
+                    'dateUpdated' => $this->dateTime()->notNull(),
+                    'uid' => $this->uid(),
+                // Custom columns in the table
+                    'assetId' => $this->integer()->notNull(),
+                    'dam_meta_key' => $this->string(255)->notNull(),
+                    'dam_meta_value' => $this->string(255)->notNull()
+                ]
+            );
+        }
+
+        return $tablesCreated;
+    }
+
+    /**
+     * Creates the indexes needed for the Records used by the plugin
+     *
+     * @return void
+     */
+    protected function createIndexes()
+    {
+    // universaldamintegrator_universaldamintegrator table
+        $this->createIndex(
+            $this->db->getIndexName(
+                '{{%universaldamintegrator_asset_metadata}}',
+                'assetId',
+                false
+            ),
+            '{{%universaldamintegrator_asset_metadata}}',
+            'assetId',
+            false
+        );
+        // Additional commands depending on the db driver
+        switch ($this->driver) {
+            case DbConfig::DRIVER_MYSQL:
+                break;
+            case DbConfig::DRIVER_PGSQL:
+                break;
+        }
+    }
+
+    /**
+     * Creates the foreign keys needed for the Records used by the plugin
+     *
+     * @return void
+     */
+    protected function addForeignKeys()
+    {
+    // universaldamintegrator_universaldamintegrator table
+        $this->addForeignKey(
+            $this->db->getForeignKeyName('{{%universaldamintegrator_asset_metadata}}', 'siteId'),
+            '{{%universaldamintegrator_asset_metadata}}',
+            'assetId',
+            '{{%assets}}',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * Populates the DB with the default data.
+     *
+     * @return void
+     */
+    protected function insertDefaultData()
+    {
+    }
+
+    /**
+     * Removes the tables needed for the Records used by the plugin
+     *
+     * @return void
+     */
+    protected function removeTables()
+    {
+    // universaldamintegrator_universaldamintegrator table
+        $this->dropTableIfExists('{{%universaldamintegrator_asset_metadata}}');
+    }
+}
